f0053bdc4c5aa90b1f10ba84a5eb6a9359381c4d
henboff <henboff@gmail.com>
2018-07-24 09:54:32 -0400

    Corrigido o redirecionamento da view. a view professores se encontra em /painel/professores

--- app/Http/Controllers/Painel/ProfessorController.php
+++ app/Http/Controllers/Painel/ProfessorController.php
@@ -7,5 +7,8 @@ use App\Http\Controllers\Controller;
 
 class ProfessorController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('painel.professores.index');
+    }
 }
