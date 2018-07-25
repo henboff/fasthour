05926dba675da6399abc847c7ca2970cd02d6e8d
henboff <henboff@gmail.com>
2018-07-25 19:11:53 -0400

    Modificado layout de listagem dos professores

--- resources/views/painel/professores/index.blade.php
+++ resources/views/painel/professores/index.blade.php
@@ -37,25 +37,23 @@
                 </div>
                 <a href="{{ route('professor.cadastro') }}" class="small-box-footer">Novo professor <i class="fa fa-arrow-circle-right"></i></a>
               </div>
-            
-        </div>
+
         <div>
-         <!-- Three columns of text below the carousel -->
-         <div class="row">
-                <div class="col-lg-4">
-                  <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
-                  <h2>Heading</h2>
-                  <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-                  <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
-                </div><!-- /.col-lg-4 -->
-              </div><!-- /.row -->
                 <h1>Listagem dos professores:</h1>
-                
-                @forelse ($professores as $prof)
-                    <p>{{ $prof->prof_nome }} </p> <button class="btn btn-sm">Editar</button>  <button class="btn btn-sm">Excluir</button> 
-                @empty
-                    <p>Não há professores cadastrados.</p>
-                @endforelse
+                <br>
+                <div class="row">
+                    @forelse ($professores as $prof)
+                        <div class="col-lg-4">
+                          <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140">
+                          <h2>Professor {{ $prof->prof_nome }}</h2>
+                          <p><a class="btn btn-sm" href="#" role="button">Editar &raquo;</a></p>
+                        </div><!-- /.col-lg-4 -->
+                    @empty
+                        <p>Não há professores cadastrados.</p>
+                    @endforelse
+                </div><!-- /.row -->
+            
+         
                 
         </div>
         
