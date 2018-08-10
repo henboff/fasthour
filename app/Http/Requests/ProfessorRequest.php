94ef3735fd8857cdbc0b1964481944e4a3620d0b
MIGUEL ARCANGELO MARTINS PEREIRA JUNIOR <04694523236@ifro.local>
2018-08-10 10:47:10 -0400

    Criada a validação de formulário do professor

---
+++ app/Http/Requests/ProfessorRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProfessorRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+    
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'prof_nome' => 'required|min:100'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'prof_nome.required' => 'É obrigatório preencher o nome do professor no seu respectivo campo.',
+            'prof_nome.min' => 'O nome do professor só pode ter no máximo 100 caracteres.'
+
+        ];
+    }
+
+}
