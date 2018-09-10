8a79abcaf458a5773bcee26ccb3a77dc777e4e52
MIGUEL ARCANGELO MARTINS PEREIRA JUNIOR <04694523236@ifro.local>
2018-09-10 17:00:11 -0400

    Tabela para montar horario alterada

--- resources/views/painel/horario/index.blade.php
+++ resources/views/painel/horario/index.blade.php
@@ -15,35 +15,24 @@
 @section('content')
     <p>Está na hora de montar!</p>
 
-    <!--Montar horário escolar-->
-
-	<div id="main_container">
-			<!-- tables inside this DIV could have draggable content -->
-			<div id="redips-drag">
-	
-				<!-- left container (table with subjects) -->
-				<div id="left">
-					<table id="table1">
-						<colgroup>
-							<col width="190"/>
-						</colgroup>
-						<tbody>
-							<tr><td class="dark"><div id="ar" class="redips-drag redips-clone ar">Empreendedorismo<br>Juliano</div><input id="b_ar" class="ar" type="button" value="" onclick="redips.report('ar')" title="Show only Arts"/></td></tr>
-							<tr><td class="dark"><div id="bi" class="redips-drag redips-clone bi">Banco de Dados<br>Kaiser</div><input id="b_bi" class="bi" type="button" value="" onclick="redips.report('bi')" title="Show only Biology"/></td></tr>
-							<tr><td class="dark"><div id="ch" class="redips-drag redips-clone ch">Des. Sistemas<br>Luiz</div><input id="b_ch" class="ch" type="button" value="" onclick="redips.report('ch')" title="Show only Chemistry"/></td></tr>
-							<tr><td class="dark"><div id="en" class="redips-drag redips-clone en">Inglês<br>Ingrid</div><input id="b_en" class="en" type="button" value="" onclick="redips.report('en')" title="Show only English"/></td></tr>
-							<tr><td class="dark"><div id="et" class="redips-drag redips-clone et">Filosofia<br>Clodoaldo</div><input id="b_et" class="et" type="button" value="" onclick="redips.report('et')" title="Show only Ethics"/></td></tr>
-							<tr><td class="dark"><div id="hi" class="redips-drag redips-clone hi">História<br>Gilson</div><input id="b_hi" class="hi" type="button" value="" onclick="redips.report('hi')" title="Show only History"/></td></tr>
-							<tr><td class="dark"><div id="it" class="redips-drag redips-clone it">Quimíca<br>Joelson</div><input id="b_it" class="it" type="button" value="" onclick="redips.report('it')" title="Show only IT"/></td></tr>
-							<tr><td class="dark"><div id="ma" class="redips-drag redips-clone ma">Matemática<br>Samanta</div><input id="b_ma" class="ma" type="button" value="" onclick="redips.report('ma')" title="Show only Mathematics"/></td></tr>
-							<tr><td class="dark"><div id="ph" class="redips-drag redips-clone ph">Física<br>Juliano de Deus</div><input id="b_ph" class="ph" type="button" value="" onclick="redips.report('ph')" title="Show only Physics"/></td></tr>
-							<tr><td class="redips-trash" title="Trash">Lixeira</td></tr>
-						</tbody>
-					</table>
-				</div><!-- left container -->
-				
-				<!-- right container -->
-				<div id="right">
+	<div class="row" id="redips-drag">
+		<div class="col-6 col-md-4">
+			<table id="table1">
+					<tbody>
+						<tr><td class="dark"><div id="ar" class="redips-drag redips-clone ar">Empreendedorismo<br>Juliano</div><input id="b_ar" class="ar" type="button" value="" onclick="redips.report('ar')" title="Show only Arts"/></td></tr>
+						<tr><td class="dark"><div id="bi" class="redips-drag redips-clone bi">Banco de Dados<br>Kaiser</div><input id="b_bi" class="bi" type="button" value="" onclick="redips.report('bi')" title="Show only Biology"/></td></tr>
+						<tr><td class="dark"><div id="ch" class="redips-drag redips-clone ch">Des. Sistemas<br>Luiz</div><input id="b_ch" class="ch" type="button" value="" onclick="redips.report('ch')" title="Show only Chemistry"/></td></tr>
+						<tr><td class="dark"><div id="en" class="redips-drag redips-clone en">Inglês<br>Ingrid</div><input id="b_en" class="en" type="button" value="" onclick="redips.report('en')" title="Show only English"/></td></tr>
+						<tr><td class="dark"><div id="et" class="redips-drag redips-clone et">Filosofia<br>Clodoaldo</div><input id="b_et" class="et" type="button" value="" onclick="redips.report('et')" title="Show only Ethics"/></td></tr>
+						<tr><td class="dark"><div id="hi" class="redips-drag redips-clone hi">História<br>Gilson</div><input id="b_hi" class="hi" type="button" value="" onclick="redips.report('hi')" title="Show only History"/></td></tr>
+						<tr><td class="dark"><div id="it" class="redips-drag redips-clone it">Quimíca<br>Joelson</div><input id="b_it" class="it" type="button" value="" onclick="redips.report('it')" title="Show only IT"/></td></tr>
+						<tr><td class="dark"><div id="ma" class="redips-drag redips-clone ma">Matemática<br>Samanta</div><input id="b_ma" class="ma" type="button" value="" onclick="redips.report('ma')" title="Show only Mathematics"/></td></tr>
+						<tr><td class="dark"><div id="ph" class="redips-drag redips-clone ph">Física<br>Juliano de Deus</div><input id="b_ph" class="ph" type="button" value="" onclick="redips.report('ph')" title="Show only Physics"/></td></tr>
+						<tr><td class="redips-trash" title="Trash">Lixeira</td></tr>
+					</tbody>
+				</table>
+		</div>
+		<div class="col-12 col-md-8">
 					<table id="table2">
 						<colgroup>
 							<col width="50"/>
@@ -154,9 +143,9 @@
 							</tr>
 						</tbody>
 					</table>
-				</div><!-- right container -->
-			</div><!-- drag container -->
-		</div><!-- main container -->
+		</div>
+	</div>
+    <!--Montar horário escolar-->
 
 @stop
 
