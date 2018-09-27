f6c35d45660ae7a578a9f0b195fcdccdab81c021
MIGUEL ARCANGELO MARTINS PEREIRA JUNIOR <04694523236@ifro.local>
2018-09-27 11:33:13 -0400

    teste

--- resources/views/painel/horario/teste.blade.php
+++ resources/views/painel/horario/teste.blade.php
@@ -18,7 +18,7 @@
                     <div class="col-xs-12">
                         <div class="box">
                             <div class="box-header with-border">
-                            <h3 class="box-title">Selecione uma turma</h3>
+                            <h3 class="box-title">Selecione a turma</h3>
                                 <div class="box-tools pull-right">
                                     <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
                                 </div>
